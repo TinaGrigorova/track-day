688a3a5356024e9a80a0890417f6bba8e6773e14
Tina Grigorova <tina.grigorova@students.codeinstitute.net>
2025-03-28 14:24:03 +0000

    add model - class car which represests the car used for the booking :

--- booking_system/models.py
+++ booking_system/models.py
@@ -12,3 +12,13 @@ class Track(models.Model):
 
     def __str__(self):
         return self.name
+    
+class Car(models.Model):
+    owner = models.ForeignKey('auth.User', on_delete=models.CASCADE, related_name='cars')
+    brand = models.CharField(max_length=50)
+    model = models.CharField(max_length=50)
+    year = models.PositiveIntegerField()
+    is_rental = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.brand} {self.model} ({self.year})"
