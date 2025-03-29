874c04d411eb17aa4d7d8c19619e98c6ed8c02ec
Tina Grigorova <tina.grigorova@students.codeinstitute.net>
2025-03-29 18:49:05 +0000

    add models to admin.py so they can be managed from admin panel .

--- booking_system/admin.py
+++ booking_system/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import Track, Car, Booking
+
+@admin.register(Track)
+class TrackAdmin(admin.ModelAdmin):
+    list_display = ('name', 'location', 'length_km')
+
+@admin.register(Car)
+class CarAdmin(admin.ModelAdmin):
+    list_display = ('brand', 'model', 'year', 'is_rental', 'owner')
+
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ('user', 'track', 'car', 'date', 'time_slot')
+    list_filter = ('track', 'date')
