c7aeff036040beab2645b51e9f6b7e0aba7cdd06
Tina Grigorova <tina.grigorova@students.codeinstitute.net>
2025-03-30 09:52:30 +0000

    add views for track ddsay booking

--- booking_system/views.py
+++ booking_system/views.py
@@ -1,6 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .forms import BookingForm
 
-# Create your views here.
-def index(request):
-    return HttpResponse("Its Connected!")
+def book_track(request):
+    """
+    Displays and processes the booking form.
+    """
+    if request.method == 'POST':
+        form = BookingForm(request.POST)
+        if form.is_valid():
+            booking = form.save(commit=False)
+            booking.user = request.user  # Assign the logged-in user
+            booking.save()
+            return redirect('booking_success')
+    else:
+        form = BookingForm()
+    return render(request, 'booking_system/book_track.html', {'form': form})
+
+def booking_success(request):
+    """
+    Simple success page after booking.
+    """
+    return render(request, 'booking_system/booking_success.html')
