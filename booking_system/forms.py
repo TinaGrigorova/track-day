724c78f709c6aa830fd621f603ac8b5fdb570126
Tina Grigorova <tina.grigorova@students.codeinstitute.net>
2025-04-12 15:35:05 +0000

    update UX design anf forms

--- booking_system/forms.py
+++ booking_system/forms.py
@@ -4,18 +4,38 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from .models import Car  
 
+from django import forms
+from .models import Booking, Track, Car
+from django.utils import timezone
+
 class BookingForm(forms.ModelForm):
+    date = forms.DateField(
+        widget=forms.DateInput(attrs={
+            'type': 'date',
+            'min': timezone.now().date().isoformat()
+        }),
+        label='Select a date'
+    )
+    time_slot = forms.CharField(
+        widget=forms.TextInput(attrs={
+            'placeholder': 'e.g. 09:00 - 10:00',
+            'class': 'form-control'
+        }),
+        label='Time slot'
+    )
+
     class Meta:
         model = Booking
         fields = ['track', 'car', 'date', 'time_slot']
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.fields['car'].queryset = Car.objects.filter(is_rental=True)
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
         self.fields['track'].queryset = Track.objects.all()
+        self.fields['car'].queryset = Car.objects.filter(is_rental=True)
+        self.fields['track'].widget.attrs.update({'class': 'form-select'})
+        self.fields['car'].widget.attrs.update({'class': 'form-select'})
+        self.fields['date'].widget.attrs.update({'class': 'form-control'})
+        self.fields['time_slot'].widget.attrs.update({'class': 'form-control'})
 
     def clean(self):
         cleaned_data = super().clean()
