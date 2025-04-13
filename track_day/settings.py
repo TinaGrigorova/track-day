eedfde7f36f144b99fb56e9c171102dfeb5ee63d
Tina Grigorova <tina.grigorova@students.codeinstitute.net>
2025-04-13 10:15:15 +0000

    install homebruew to fix heroku migraions

--- track_day/settings.py
+++ track_day/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-6n#rc)ok*uhsocqn-^1q=4ja&43a(k8yt3o-srbnb1-n(f385k"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['track-day-b7bd1e661185.herokuapp.com', '127.0.0.1', 'localhost']
 
